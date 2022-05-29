8796c93865e121e0de6feb0a659f54f5fd2c9195
quangnphan <qp2194@gmail.com>
2022-05-29 16:09:01 -0500

    push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import { Bar } from "react-chartjs-2";
+import "chart.js/auto";
+import data from "./data.json";
 
 function App() {
+  const [ chart, setChart ] = useState({
+    labels: data.map((data)=> data.day),
+    datasets: [
+      {
+        labels: "",
+        data: data.map((data)=> data.amount),
+        backgroundColor: ["hsl(10, 79%, 65%)"],
+        hoverBackgroundColor: ["hsl(186, 34%, 60%)"],
+        borderRadius: 3,
+      }
+    ],
+  });
+  const [chartOptions,setChartOptions] = useState({
+    scales: {
+      y: {
+        ticks: {
+          display: false,
+        },
+        grid: {
+          drawBorder: false,
+          display: false,
+        },
+      },
+      x: {
+        grid: {
+          drawBorder: false,
+          display: false,
+        },
+      },
+    },
+    plugins: {
+      legend: false,
+    },
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="component">
+      <div className="header">
+        <div className="balance">
+          <p>My balance</p>
+          <span>$921.48</span>
+        </div>
+        <div className="logo">
+          <img src={logo} alt="logo"></img>
+        </div>
+      </div>
+      <div className="body">
+        <h3>Spending - Last 7 days</h3>
+        <div className="bar-chart">
+          <Bar data={chart} options={chartOptions}/>
+        </div>
+        <hr></hr>
+        <div className="bottom">
+          <div className="bottom-left">
+            <p>Total this month</p>
+            <span>$478.33</span>
+          </div>
+          <div className="bottom-right">
+            <span>+2.4%</span>
+            <p>from last month</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
